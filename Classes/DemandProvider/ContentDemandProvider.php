80c39f5c9e39e980983fae618c8d2f9f096aa0dd
Mats Svensson <mats@pixelant.se>
2021-02-23 12:02:03 +0100

    [WIP] add services file for dependency injections

--- Classes/DemandProvider/ContentDemandProvider.php
+++ Classes/DemandProvider/ContentDemandProvider.php
@@ -6,4 +6,11 @@ namespace Pixelant\Demander\DemandProvider;
 
 class ContentDemandProvider implements DemandProviderInterface
 {
+    /**
+     * @return array
+     */
+    public function getDemand(): array
+    {
+        return [];
+    }
 }
--- Classes/DemandProvider/PageDemandProvider.php
+++ Classes/DemandProvider/PageDemandProvider.php
@@ -6,4 +6,11 @@ namespace Pixelant\Demander\DemandProvider;
 
 class PageDemandProvider implements DemandProviderInterface
 {
+    /**
+     * @return array
+     */
+    public function getDemand(): array
+    {
+        return [];
+    }
 }
